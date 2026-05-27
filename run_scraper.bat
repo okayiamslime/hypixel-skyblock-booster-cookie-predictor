00f32613ae680b8f02a6c5ee9f753e388ccc5437
okayiamslime <77953509+okayiamslime@users.noreply.github.com>
2026-05-28 00:23:16 +0800

    Update run_scraper.bat

--- run_scraper.bat
+++ run_scraper.bat
@@ -1,3 +1,3 @@
 cd /d C:\Users\YourUsername\OneDrive\Documents\Python\
-"C:\Users\YourUsername\AppData\Local\Programs\Python\Python311\python.exe" "Bazaar Price Fetcher.py"
+"C:\Users\YourUsername\AppData\Local\Programs\Python\Python311\python.exe" "Bazaar Predict.py"
 pause
